bdeb72207e9b435672e8d9ce3f860ac86830a6fd
tasiten <99870120+tasiten@users.noreply.github.com>
2024-08-31 22:29:47 +0900

    Update install.sh

--- zshFile/岩島くんのzshrcファイル/zshrc/install.sh
+++ zshFile/岩島くんのzshrcファイル/zshrc/install.sh
@@ -2,9 +2,9 @@ yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/mast
 install_pid=$!
 wait $install_pid
 
-[ ! -d ~/.local/share/fonts ] && mkdir -p ~/.local/share/fonts
-mv HackGen_NF/ ~/.local/share/fonts
-fc-cache /var/cache/fontconfig/
+# [ ! -d ~/.local/share/fonts ] && mkdir -p ~/.local/share/fonts
+# mv HackGen_NF/ ~/.local/share/fonts
+# fc-cache /var/cache/fontconfig/
 
 cp zshrc ~/.zshrc
 cp fuya-omi.zsh-theme ~/.oh-my-zsh/themes/
